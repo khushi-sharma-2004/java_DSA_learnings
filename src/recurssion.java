64e70dc894b8b109ec810f75de65340915584346
khushi-sharma-2004 <khushisharma.29102004@gmail.com>
2025-11-02 01:25:20 +0000

    recurssion

--- src/recurssion.java
+++ src/recurssion.java
@@ -1,14 +1,14 @@
-class main{
+class recurssion{
     public static void main(String[] args){
-      print(1);
+      print(1);//function call
     }
     static void print(int n ){
-        if(n>=6){
+        if(n>=6){ // base condition
             return ;
         }
         else{
             System.out.println(n);
-            print(n+1);
+            print(n+1);//fuction calling it  again and again
         }
     }
 }
