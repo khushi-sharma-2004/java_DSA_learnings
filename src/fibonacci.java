6514d699b530ae15af2c4e3deeddf292c9e21466
khushi-sharma-2004 <khushisharma.29102004@gmail.com>
2025-11-02 03:42:34 +0000

    fibonacci

---
+++ src/fibonacci.java
@@ -0,0 +1,14 @@
+
+class fibonacci{
+    public static void main(String[] args){
+       System.out.println(f(8));
+    }
+    static int f(int n ){
+        if(n<2){
+            return n;
+        }
+        else{
+            return f(n-1)+f(n-2);
+        }
+    } 
+}
