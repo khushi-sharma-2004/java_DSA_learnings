cb624c081d4249ac03049615f87fdc2b648e92d8
khushi-sharma-2004 <khushisharma.29102004@gmail.com>
2025-11-27 04:21:22 +0000

    pattern 2

--- Patterns/pattern2.java
+++ Patterns/pattern2.java
@@ -3,19 +3,16 @@ class pattern2 {
         print(4, 0);
     }
 
-    // r = number of rows left
-    // c = number of stars printed in current row
     public static void print(int r, int c) {
-        if (r == 0) {                 // No rows left
-            return;
-        }
+        if (r == 0) return;
 
-        if (c < r) {                  // Print stars in the row
-            System.out.print("*");
+        if (c < r) {
             print(r, c + 1);
-        } else {                      // Move to next row
-            System.out.println();
+            System.out.print("*");
+        }
+        else {
             print(r - 1, 0);
+            System.out.println();
         }
     }
 }
